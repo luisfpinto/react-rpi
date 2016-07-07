1927500584d9414bae2d32a1f95cffa5aa4b4f28
luisfpinto <luisfpintos@gmail.com>
2016-07-07 13:00:14 +0000

    pin control works

--- index.js
+++ index.js
@@ -20,18 +20,34 @@ var server = app.listen(8000, function(){
 
 app.post('/', function (req, res) {
 
-	console.log(req.body)
-
 	var action = req.body.action
 	var pin = req.body.pin
-	var value = req.value
+	var value = 0 
+	
+	//Control variables
+	var setup = false
+	var prevPin = 0 
+	//If pin has been already set up we mustn't do that again. 
+	if(prevPin != pin) {
+		setup = false
+		prevPin = pin
+	}
+
+	if(req.body.value === 'true' || req.body.value === 'on') value = 1
 
 	if(action === 'read') {
-		gpio.setup(pin, gpio.DIR_IN, helper.readInput(pin));
+		if(!setup) {
+			gpio.setup(pin, gpio.DIR_IN, helper.readInput.bind(this, pin));
+			setup = true
+		}
+		else helper.readInput(pin)
 	}
 
 	if(action === 'write') {
-		gpio.setup(pin, gpio.DIR_OUT, helper.write(pin, value));
-
+		if(!setup) {
+			gpio.setup(pin, gpio.DIR_OUT, helper.write.bind(this, pin, value));
+			setup = true
+		}
+		else helper.write(pin, value)
 	}
 })
--- src/helper.js
+++ src/helper.js
@@ -1,21 +1,16 @@
 var gpio = require('rpi-gpio');
-
 var helper = module.exports = {}
 
 
 helper.readInput = function (pin) {
-	console.log('Reading')
     gpio.read(pin, function(err, value) {
         console.log('The value is ' + value);
     });
 }
 
 helper.write = function (pin, value) {
-	console.log('Writing')
-	console.log('Pin: ', pin)
-	console.log('Value: ', value)
     gpio.write(pin, value, function(err) {
         if (err) throw err;
-        console.log('Written to pin');
+        console.log('Written to pin: ', value);
     });
 }
